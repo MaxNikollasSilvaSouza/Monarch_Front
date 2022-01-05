05340ce6709ce1869094c8339db7bf6fc8af288b
MaxNikollasSilvaSouza <maxnikollas@gmail.com>
2022-01-05 19:30:05 -0300

    [Max - Versão 1.18]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AjudaComponent } from './ajuda/ajuda.component';
+
 
 import { SectionComponent } from './section/section.component';
 
 
 const routes: Routes = [
 
-  {path: '', component: SectionComponent},
-  {path:'/ajuda',component:AjudaComponent}  
+  {path: '', component: SectionComponent}
+  
 
 
 
